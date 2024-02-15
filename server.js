8add1bd5f680ec44b4f2fa91f4118fd055d2117a
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-02-15 11:53:39 -0800

    connect to jawsdb

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const path = require('path');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./Develop/config/connection');
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -46,23 +47,23 @@ app.use(express.static(path.join(__dirname, 'Develop/public')));
 app.use('/', authRoutes);
 app.use('/', homeRoute);
 
-const YOUR_DOMAIN = 'http://localhost:4242';
+// const YOUR_DOMAIN = 'http://localhost:4242';
 
-app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                price: '{{PRICE_ID}}',
-                quantity: 1,
-            },
-        ],
-        mode: 'payment',
-        success_url: `${YOUR_DOMAIN}/success.html`,
-        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-    });
+// app.post('/create-checkout-session', async (req, res) => {
+//     const session = await stripe.checkout.sessions.create({
+//         line_items: [
+//             {
+//                 // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+//                 price: '{{PRICE_ID}}',
+//                 quantity: 1,
+//             },
+//         ],
+//         mode: 'payment',
+//         success_url: `${YOUR_DOMAIN}/success.html`,
+//         cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+//     });
 
-    res.redirect(303, session.url);
-});
+//     res.redirect(303, session.url);
+// });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(3001, () => console.log('Running on port 3001'));
