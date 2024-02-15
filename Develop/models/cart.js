a370533f892462d7199f7f8dc2462d9f15ba6f75
Matt Mullen <mhmullen@icloud.com>
2024-02-15 09:08:54 -0800

    added cart button

--- Develop/models/cart.js
+++ Develop/models/cart.js
@@ -1,28 +1,44 @@
-const { Product, Datatypes } = require('sequelize');
-const { sequelize } = require('../config/db');
-import { DataTypes } from 'sequelize';
+// const { Product, Datatypes } = require('sequelize');
+// const { sequelize } = require('../config/db');
+// import { DataTypes } from 'sequelize';
 
-class cart extends Model{ }
+// class cart extends Model{ }
 
-cart.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
+// cart.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             autoIncrement: true,
+//             primaryKey: true
 
-        },
-        product_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: Product,
-                key: 'id'
-            },
-        },
-        quantity: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 1
-        },
-    })
+//         },
+//         product_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             references: {
+//                 model: Product,
+//                 key: 'id'
+//             },
+//         },
+//         quantity: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             defaultValue: 1
+//         },
+// })
+const cart = [];
+module.exports = class cart {
+    static save(product, quantity) {
+        If(cart) {
+            
+        } else {
+            cart = { products: [], totalPrice: 0 };
+            product.qty = 1
+            cart.products.push({ product, quantity });
+            cart.totalPrice += product.price;
+        }
+    }
+    static getCart() {
+        return cart;
+    }
+}
